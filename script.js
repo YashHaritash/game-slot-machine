b15ac70c2891302f668039e5f5f54bc483cb9d99
YashHaritash <yashharitash4@gmail.com>
2024-05-19 14:30:17 +0530

    updates in js file shorter code

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 let value1 = document.getElementById('value1');
 let value2 = document.getElementById('value2');
 let value3 = document.getElementById('value3');
-// let value = document.getElementsByClassName('value');
 let result = document.getElementById('result');
 let inpSpeed = document.getElementById('inpSpeed');
-
 
 let values = [
     '😇', '😭', '😃', '😎', '😂', '😋', '😡'
@@ -17,107 +15,51 @@ function getRandomValue() {
 let btn = document.getElementById('btn');
 let intervalId;
 
-
-let flag = false;
-
-btn.onclick = function() {
-    if(flag){return;}
-    flag = true;
-    result.innerText = ' ';
+function updateAnimation(newSpeed) {
+    if (intervalId) clearInterval(intervalId);
     intervalId = setInterval(() => {
         value1.innerText = getRandomValue();
         value2.innerText = getRandomValue();
         value3.innerText = getRandomValue();
-    }, 100);
-    value1.style.animationDuration = '0.1s';
-    value2.style.animationDuration = '0.1s';
-    value3.style.animationDuration = '0.1s';
+    }, 100 * newSpeed);
+}
+let temp = 800;
+let flag = false;
 
+btn.onclick = () => {
+    if(flag){return;}
+    flag = true;
+    let Nspeed = 0;
+    document.documentElement.style.setProperty('--speed', 1);
+    updateAnimation(1);
 
     setTimeout(()=>{
-        setTimeout(()=>{
-            value1.style.animationDuration = '0.2s';
-            value2.style.animationDuration = '0.2s';
-            value3.style.animationDuration = '0.2s';
-        },500)
-        setTimeout(()=>{
-            clearInterval(intervalId)
-            intervalId = setInterval(() => {
-                value1.innerText = getRandomValue();
-                value2.innerText = getRandomValue();
-                value3.innerText = getRandomValue();
-            }, 300);
-            value1.style.animationDuration = '0.3s';
-            value2.style.animationDuration = '0.3s';
-            value3.style.animationDuration = '0.3s';
-        },1000)
-        setTimeout(()=>{
-            value1.style.animationDuration = '0.4s';
-            value2.style.animationDuration = '0.4s';
-            value3.style.animationDuration = '0.4s';
-        },1500)
-        setTimeout(()=>{
-            clearInterval(intervalId)
-            intervalId = setInterval(() => {
-                value1.innerText = getRandomValue();
-                value2.innerText = getRandomValue();
-                value3.innerText = getRandomValue();
-            }, 400);
-            value1.style.animationDuration = '0.6s';
-            value2.style.animationDuration = '0.6s';
-            value3.style.animationDuration = '0.6s';
-        },2000)
-        setTimeout(()=>{
-            clearInterval(intervalId)
-            intervalId = setInterval(() => {
-                value1.innerText = getRandomValue();
-                value2.innerText = getRandomValue();
-                value3.innerText = getRandomValue();
-            }, 600);
-            value1.style.animationDuration = '0.8s';
-            value2.style.animationDuration = '0.8s';
-            value3.style.animationDuration = '0.8s';
-        },2500)
-        setTimeout(()=>{
-            clearInterval(intervalId);
-            value1.style.animationDuration = '0s';
-            value2.style.animationDuration = '0s';
-            value3.style.animationDuration = '0s';
-            if(value1.innerText == value2.innerText && value2.innerText == value3.innerText){
-                result.innerText = 'You Won!!!';
+        let id = setInterval(() => {
+            if (Nspeed >= 7) {
+                clearInterval(id);
+                clearInterval(intervalId);
+                flag = false;
+                document.documentElement.style.setProperty('--speed', 0);
+
+                if(value1.innerText == value2.innerText && value2.innerText == value3.innerText){
+                    result.innerText = 'You Won!!!';
+                }
+                else{
+                    result.innerText = 'Try Again';
+                }
+            } else {
+                Nspeed++;
+                document.documentElement.style.setProperty('--speed', Nspeed);
+                updateAnimation(Nspeed);
+                
             }
-            else{
-                result.innerText = 'Try Again';
-            }
-            flag = false;
-        },3000)
+        }, temp);
     },2000);
     
+    
 };
-
 document.addEventListener('keypress',(event)=>{
     if(event.keyCode == 32){
         btn.onclick();
     }
 })
-
-
-// function updateAnimation(newSpeed){
-//     if(intervalId) clearInterval(intervalId);
-//     intervalId = setInterval(() => {
-//         value1.innerText = getRandomValue();
-//         value2.innerText = getRandomValue();
-//         value3.innerText = getRandomValue();
-//     }, 1000/newSpeed);
-// }
-
-
-// inpSpeed.onchange = (event)=>{
-//     document.documentElement.style.setProperty('--speed',event.target.value);
-//     updateAnimation(event.target.value)
-// }
-// intervalId = setInterval(() => {
-//     value1.innerText = getRandomValue();
-//     value2.innerText = getRandomValue();
-//     value3.innerText = getRandomValue();
-// }, 100);
